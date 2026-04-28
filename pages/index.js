6ac40ad2acf7642b554089c67d0ca8fd20cc8bb0
ferolero <ferolero@gmail.com>
2026-04-28 17:56:48 -0600

    updated the bio

--- pages/index.js
+++ pages/index.js
@@ -22,9 +22,8 @@ export default function Home({allPostsData}) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>I dfj ej kf jek jdk jfaksdjf kasj faej kfj f</p>
-        <p>(esto es solamente una simulación)</p>
-        <p>[ora weyyy!]</p>
+        <p>[This is a Blog having some interesting entries regarding NextJS and some cool stuff]</p>
+        <p>(esto es solamente una simulación - esto es solamente una simulación - esto es solamente una simulación)</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
